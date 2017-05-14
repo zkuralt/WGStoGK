aecc4985600e1766ce2fdeabf58e1e1bb7cab127
Zan Kuralt <zan.kuralt@gmail.com>
2017-05-14 09:34:20 +0200

    Some layout (ui.r) modifications and addition of a leaflet map displaying converted coordinates.

--- server.R
+++ server.R
@@ -2,15 +2,35 @@ library(shiny)
 library(sp)
 library(measurements)
 library(rgdal)
+library(leaflet)
 source("custom_functions.R")
 
 shinyServer(function(input, output) {
+  ## Converts coords to GK and back to WGS in order to display on map
+  origInput <- reactive({
+    x <- read.table(text = input$coords, stringsAsFactors = FALSE)
+    x <- sapply(x, gsub, pattern = ",", replacement = ".")
+    x <- sapply(x, gsub, pattern = "°|'|''", replacement = " ")
+    x <- anyWGStoDec(x)
+    x <- data.frame(matrix(x, ncol = 2, byrow = TRUE))
+    names(x) <- c("lat", "long")
+    coordinates(x) <- c("long", "lat")
+    # coordinates(x) <- ~long+lat
+    
+    # Define input and output coordinate systems.
+    proj4string(x) <- CRS("+init=epsg:4326") # WGS 84
+    wgs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    CRS.new <- CRS("+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=bessel +towgs84=426.9,142.6,460.1,4.91,4.49,-12.42,17.1 +units=m +no_defs")
+    x.new <- spTransform(x, CRS.new)
+    x.old <- spTransform(x.new, wgs)
+    coordinates(x.old)
+})  
   
-  
+  ## Converts coords to GK
   coordInput <- reactive({
     x <- read.table(text = input$coords, stringsAsFactors = FALSE)
-    x <- sapply(x, gsub, pattern = "°|'|''", replacement = " ")
     x <- sapply(x, gsub, pattern = ",", replacement = ".")
+    x <- sapply(x, gsub, pattern = "°|'|''", replacement = " ")
     x <- anyWGStoDec(x)
     x <- data.frame(matrix(x, ncol = 2, byrow = TRUE))
     names(x) <- c("lat", "long")
@@ -26,7 +46,6 @@ shinyServer(function(input, output) {
     
   })
   
-  
   output$coords <- renderText({
     paste("Original coordinates:", input$coords)
   })
@@ -36,4 +55,14 @@ shinyServer(function(input, output) {
     coordInput()
   })
   
+  output$leaflet <- renderLeaflet({
+    
+    leaflet() %>%
+      addProviderTiles(providers$OpenStreetMap.Mapnik,
+                       options = providerTileOptions(noWrap = TRUE)
+                       
+      ) %>%
+      addMarkers(data = origInput())
+  
 })
+}) 
--- ui.R
+++ ui.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(leaflet)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
 
 shinyUI(fluidPage(
   titlePanel("WGStoGK - version 0.5"),
@@ -9,6 +13,8 @@ shinyUI(fluidPage(
       # label = "Select preffered CRS",
       # choices = list("EPSG:3787", "EPSG:3912",
       # "EPSG:3794")),
+      h5(strong("Points on map")),
+      leafletOutput("leaflet"),
       h6("CRS used: +proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=bessel
          +towgs84=426.9,142.6,460.1,4.91,4.49,\n-12.42,17.1 +units=m +no_defs")
       ),
@@ -17,6 +23,7 @@ shinyUI(fluidPage(
       # textOutput("selected.format"),
       textOutput("coords"),
       # textOutput("new.coords")
+      h5(strong("Converted coordinates")),
       tableOutput("new.coords")
     )
   )
