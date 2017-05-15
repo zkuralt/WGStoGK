bac31487aa854c2bbc4b9151f0476079cbb48a7a
Zan Kuralt <zan.kuralt@gmail.com>
2017-05-15 21:17:35 +0200

    Changed app layout, added input option for CSV file upload and convert buttons.

--- ui.R
+++ ui.R
@@ -2,32 +2,73 @@ library(shiny)
 library(leaflet)
 
 shinyUI(fluidPage(
+  
   titlePanel("WGStoGK - version 0.6"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput("coords", label = "Coordinates input", value = "46.05116 14.46990"),
-      # selectInput("CRS",
-      #             label = "Select preffered CRS",
-      #             choices = list("EPSG:3787", "EPSG:3912",
-      #                            "EPSG:3794")),
-      # fileInput("file", label = "CSV file input", multiple = FALSE),
-      h6("CRS used: +proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=bessel
-         +towgs84=426.9,142.6,460.1,4.91,4.49,\n-12.42,17.1 +units=m +no_defs")
-      ),
+  
+  fluidRow(
+    column(2,
+           textInput("coords", label = "Coordinates input", value = "46.05116 14.46990"),
+           actionButton("action", label = "Convert"), ### Make button display converted coords.
+           ### add text from thinkpad
+           hr(),
+           fileInput("file", label = "CSV file input", multiple = FALSE),
+           selectInput("sep", "Separator", choices = c(".",",",";")),
+           selectInput("header", "Header", choices = c(TRUE, FALSE)),
+           actionButton("action", label = "Convert") ### Make button display converted coords.
+           # selectInput("crs",
+           #             label = "Select preffered CRS",
+           #             choices = list("EPSG:3787", "EPSG:3912",
+           #                            "EPSG:3794")),
+    ),
+    column(6,
+           br(),
+           leafletOutput("leaflet"),
+           hr(),
+           h6("CRS used: +proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=bessel
+              +towgs84=426.9,142.6,460.1,4.91,4.49,-12.42,17.1 +units=m +no_defs")),
+    column(2,
+           h5(strong("Original coordinates (WGS)"),
+              tableOutput("coords"))),
     
-    mainPanel(
-      fluidRow(
-        splitLayout(cellWidths = c("50%", "50%"), 
-                    h5(strong("Original coordinates (WGS)")), 
-                    h5(strong("Converted coordinates (GK)"))
-      ),
-      fluidRow(
-        splitLayout(cellWidths = c("50%", "50%"), tableOutput("coords"), tableOutput("new.coords"))
-      ),
-      h5(strong("Points on map")),
-        leafletOutput("leaflet")
-     
-      
+    column(2,
+           h5(strong("Converted coordinates (GK)"),
+              tableOutput("new.coords"))
+           
     )
-  )
   )))
+
+
+
+# library(shiny)
+# library(leaflet)
+# 
+# shinyUI(fluidPage(
+#   titlePanel("WGStoGK - version 0.6"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       textInput("coords", label = "Coordinates input", value = "46.05116 14.46990"),
+#       # selectInput("CRS",
+#       #             label = "Select preffered CRS",
+#       #             choices = list("EPSG:3787", "EPSG:3912",
+#       #                            "EPSG:3794")),
+#       # fileInput("file", label = "CSV file input", multiple = FALSE),
+#       h6("CRS used: +proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=bessel
+#          +towgs84=426.9,142.6,460.1,4.91,4.49,\n-12.42,17.1 +units=m +no_defs")
+#       ),
+#     
+#     mainPanel(
+#       fluidRow(
+#         splitLayout(cellWidths = c("50%", "50%"), 
+#                     h5(strong("Original coordinates (WGS)")), 
+#                     h5(strong("Converted coordinates (GK)"))
+#       ),
+#       fluidRow(
+#         splitLayout(cellWidths = c("50%", "50%"), tableOutput("coords"), tableOutput("new.coords"))
+#       ),
+#       h5(strong("Points on map")),
+#         leafletOutput("leaflet")
+#      
+#       
+#     )
+#   )
+#   )))
