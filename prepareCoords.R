91e77d21885e354d6303361d1e537ed3feec5cdf
Roman Luštrik <romunov@gmail.com>
2017-05-19 12:51:55 +0200

    removed debugging statements

--- prepareCoords.R
+++ prepareCoords.R
@@ -1,6 +1,5 @@
 prepareCoords <- function(x) {
   x <- sapply(x, gsub, pattern = ",", replacement = ".")
-  print("\U00B0|'|''")
   x <- sapply(x, gsub, pattern = "\U00B0|'|''", replacement = " ")
   x
 }
